933529fce2dd65f36eeb7b0bc22dfcbda4095f39
David Liang <davidxliang@gmail.com>
2016-12-10 15:37:10 -0800

    there's an awesome 2 line solution using Counter

---
+++ Ini_6.py
@@ -0,0 +1,9 @@
+holder = {} #creating a key-value dictionary 
+sent = 'When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom let it be And in my hour of darkness she is standing right in front of me Speaking words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the broken hearted people living in the world agree There will be an answer let it be For though they may be parted there is still a chance that they will see There will be an answer let it be Let it be let it be let it be let it be There will be an answer let it be Let it be let it be let it be let it be Whisper words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the night is cloudy there is still a light that shines on me Shine until tomorrow let it be I wake up to the sound of music Mother Mary comes to me Speaking words of wisdom let it be Let it be let it be let it be yeah let it be There will be an answer let it be Let it be let it be let it be yeah let it be Whisper words of wisdom let it be'
+for word in sent.split(' '):
+    if word in holder:
+        holder[word] += 1
+    else:
+        holder[word] = 1 #if there's no other words available in there  
+for key,value in holder.items():
+    print key, value
