92452ac8808f4c738238c44f7bc95365807cc371
David Liang <davidxliang@gmail.com>
2016-12-15 15:15:40 -0800

    adding description to make sure it makes sense in my head and what not

--- SD_BINF/Clump_FrequentWord.py
+++ SD_BINF/Clump_FrequentWord.py
@@ -1,14 +1,23 @@
 #4 - finding the clump using frequentWord.py algorithms 
 #getting in this habit of creating a main 
+#disclaimer: not yet modified with frequency arrays or patternToNumber.py, will do so once i finish those sections 
 def main(): 
-    #main code in here 
+    #main code in here
+    genome_input = input("What to analyze")
+    output_size = input("What length of the sequence do you want to print out (based on frequentWords)")
+    starting_position = input("starting position in the genome?")
+    num_appearance = input("how many times it has to appear before we include it?") 
+    def Clump_hotspot(genome_input, output_size, starting_position, num_appearance)
 
-def Clump_hotspot(genome_string, frequent_kmers, L_window_space, t_end_window):
+
+def Clump_hotspot(genome_string, frequent_kmers, L_window_space, t_times_appeared):
     #definitions: genome_string = the string inputted, frequent_kmers -> what is printed out from frequent words but a bit modified here
-    # L_window -> interpreted as the starting position of the genome string, t_end_window -> the final index/position of the L_window or the index of 'e' in 'apple'  
+    # L_window -> interpreted as the starting position of the genome string, t_times_appeared -> pretty obvious here, but how many times it appeared in the selected region
+     
+    
 
 
 if __name__ == "__main__":
-    #making my python coding habit a bit nicer to look at oohhoohhoh 
+    #making my python coding habit a bit nicer to look at oohhoohhoh
     main()
     
