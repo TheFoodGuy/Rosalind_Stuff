8c30ee0c51821f0bbfdafe69c6c374bc0bd5585a
David Liang <davidxliang@gmail.com>
2016-12-15 00:01:27 -0800

    adding in code from the bioinformatics specialization class from ucsd

---
+++ Data_Structure.py
@@ -0,0 +1,16 @@
+#testing out the stack data structure here 
+# from Stack import * #from Stack.py import everything. 
+# s = Stack() 
+# print(s.isEmpty())
+# s.push(4) 
+# s.push('dog')
+# print(s.peek())
+# print("This is the size of the Stack right now : "  + str(s.size()))
+
+#testing out the queue data structure here 
+from Queue import * 
+q = Queue()
+q.enqueue(4) 
+print(q.size())
+q.dequeue()
+print(q.isEmpty())
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Rosalind_Stuff
+Anyways, this is practice for future bioinformatics courses since my school uses mainly python. 
