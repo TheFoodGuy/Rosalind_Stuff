b882c6e62c2890f3a19ec36ddbdfe67aaf761ec7
David Liang <davidxliang@gmail.com>
2016-12-15 00:37:41 -0800

    adding FrequentWord.py which checks for the most common sequence in a string

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Rosalind_Stuff
 Anyways, this is practice for future bioinformatics courses since my school uses mainly python. 
+Also, I discovered this awesome online class from ucsd or something seemingly close to BME 205 from ucsc 
+Disclaimer: Beginner in python here 
---
+++ SD_BINF/FrequentWord.py
@@ -0,0 +1,25 @@
+#2 - checking for how frequent a particular pattern appears in a string 
+genome_string = 'TTGCACCAGGAAATGCCCAAGCGAGGAAATGTTGCACCGTGGGTGAGGAAATGCCCAAGCGTTGCACCCCCAAGCGAGGAAATGCGTATATGGCCCAAGCGCGTATATGGCCCAAGCGTTGCACCGTGGGTGGTGGGTGGTGGGTGAGGAAATGCGTATATGGAGGAAATGCCCAAGCGCCCAAGCGGTGGGTGGTGGGTGAGGAAATGTTGCACCAGGAAATGCCCAAGCGAGGAAATGAGGAAATGGTGGGTGGTGGGTGAGGAAATGTTGCACCTTGCACCCGTATATGGTTGCACCAGGAAATGCGTATATGGTTGCACCGTGGGTGCGTATATGGCCCAAGCGTTGCACCTTGCACCCGTATATGGGTGGGTGTTGCACCAGGAAATGAGGAAATGGTGGGTGTTGCACCCGTATATGGGTGGGTGCCCAAGCGCGTATATGGTTGCACCGTGGGTGCCCAAGCGAGGAAATGTTGCACCCGTATATGGCGTATATGGCCCAAGCGTTGCACCTTGCACCTTGCACCAGGAAATGCCCAAGCGTTGCACCTTGCACCTTGCACCCCCAAGCGCGTATATGGGTGGGTGCGTATATGGCCCAAGCGAGGAAATGCCCAAGCGTTGCACCCGTATATGGCGTATATGGTTGCACCTTGCACCAGGAAATGCGTATATGGAGGAAATGTTGCACCTTGCACCGTGGGTGAGGAAATGTTGCACCGTGGGTGAGGAAATGCGTATATGGTTGCACCGTGGGTGAGGAAATGGTGGGTGCGTATATGGGTGGGTGCCCAAGCGTTGCACCTTGCACCGTGGGTGTTGCACCCGTATATGGAGGAAATGTTGCACCGTGGGTG'
+genome_pattern = 11
+#genome_pattern is
+def FrequentWords(text, k) : 
+    #text - > genome_string 
+    #k - > genome_pattern value 
+    genome_count = {}
+    for i in range(len(text) - k + 1): # < - from the instruction here
+        pattern = text[i:k+i] 
+        if pattern in genome_count: 
+            genome_count[pattern] += 1
+        else: 
+            genome_count[pattern] = 1 
+        max_count = max(genome_count.values())
+    
+    #target = [] # it prints the sequences with the greatest values, should be an even number of sequences 
+    for k in genome_count: 
+        if genome_count[k] == max_count: 
+            print(k)
+            #target.append(k) #there's really no point in doing this tho
+            
+
+
+FrequentWords(genome_string, genome_pattern)
